93463f692f7e62662d12da2d4e108d1da3645b4a
Pc <meysamhadeli@gmail.com>
2023-08-01 02:24:30 +0330

    feat: Add retry failure and dashboard for cap

--- src/BuildingBlocks/CAP/Extensions.cs
+++ src/BuildingBlocks/CAP/Extensions.cs
@@ -21,8 +21,19 @@ public static class Extensions
         {
             x.UseInMemoryStorage();
             x.UseInMemoryMessageQueue();
+            
+            x.UseDashboard();
+            x.FailedRetryCount = 5;
+            x.FailedThresholdCallback = failed =>
+            {
+                var logger = failed.ServiceProvider.GetService<ILogger>();
+                logger?.LogError(
+                    $@"A message of type {failed.MessageType} failed after executing {x.FailedRetryCount} several times,
+                        requiring manual troubleshooting. Message name: {failed.Message.GetName()}");
+            };
+            x.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
         });
-        
+
         services.Scan(s =>
             s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
                 .AddClasses(c => c.AssignableTo(typeof(ICapSubscribe)))
